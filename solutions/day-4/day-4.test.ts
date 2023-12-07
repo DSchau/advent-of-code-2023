71033db7e1d498b8b128942c92bd52ce2553cf06
Dustin Schau <dustinschau@gmail.com>
2023-12-07 15:02:31 -0800

    chore: part one

--- solutions/day-4/day-4.test.ts
+++ solutions/day-4/day-4.test.ts
@@ -1,7 +1,11 @@
 import { expect, test } from "bun:test";
-import { sample } from "./input";
+import { day_one, sample } from "./input";
 import { getScore } from "./day-4";
 
 test('it works for sample input', () => {
-  expect(getScore(sample)).toEqual(0)
+  expect(getScore(sample)).toEqual(13)
+})
+
+test('it works for puzzle input', () => {
+  expect(getScore(day_one)).toEqual(20107)
 })
--- solutions/day-4/day-4.ts
+++ solutions/day-4/day-4.ts
@@ -1,4 +1,39 @@
+type WinningLookup = {
+  [key: string]: boolean;
+}
+
+interface Game {
+ id: number;
+ options: number[];
+ winning: WinningLookup;
+}
+
 export function getScore(input: string): number {
   const rows = input.trim().split('\n')
-  return 0
+
+  const game: Game[] = rows.map(row => {
+    const id: number = row.match(/Card\s(\d+)/)?.pop() as any
+    
+    let [winning, options] = row.replace(/Card[^:]+:/, '').trim().split('|').map(part => part.trim())
+
+    winning = winning.match(/\d+/g)?.reduce((merged: WinningLookup, num: string) => {
+      merged[num] = true
+      return merged
+    }, {}) as any
+    options = options.match(/\d+/g)?.map(part => parseInt(part, 10)) as any
+
+    return {
+      id,
+      options,
+      winning
+    }
+  })
+
+  return game.reduce((total: number, game: Game) => {
+    const winning_numbers = game.options
+      .filter(option => game.winning[option])
+
+    const score = winning_numbers.length === 0 ? 0 : Math.pow(2, winning_numbers.length - 1)
+    return total + score
+  }, 0)
 }
